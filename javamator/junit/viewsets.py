56b3ab4cffb584036f03b9e19771676fb42bc0a5
Simon Chervenak <sarras305@gmail.com>
2018-10-22 10:07:46 -0400

    fixed creation error

--- javamator/junit/viewsets.py
+++ javamator/junit/viewsets.py
@@ -1,7 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,filters
 from .models import JUnit
 from .serializers import JUnitSerializer
 
 class JUnitViewSet(viewsets.ModelViewSet):
     queryset = JUnit.objects.all()
     serializer_class = JUnitSerializer
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('class_name', 'package_name', 'body')

5a148cedf50d489b3d85c7ad91bb0cf6b43a1585
Simon Chervenak <sarras305@gmail.com>
2018-10-22 09:44:11 -0400

    added junit model

--- javamator/main/models.py
+++ javamator/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class JUnit(models.Model):
+    class_name = models.CharField(max_length=250)
+    package_name = models.CharField(max_length=250)
+    body = models.TextField()

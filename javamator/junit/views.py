34e9c36adede5e9b345a1fede10819f5c5bcac33
Simon Chervenak <sarras305@gmail.com>
2018-10-22 21:58:53 -0400

    starting to add test views

--- javamator/junit/views.py
+++ javamator/junit/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Teacher
+import requests as req
 
 # Create your views here.
 def login(req):
-    return render(req, "index.html", {'login': 'true', 'login_success': 'true'})
+    succeeded = False
+    for obj in Teacher.objects.all():
+        if obj.name == req.POST['username'] and obj.assword == req.POST['password']:
+            succeeded = True
+    return render(req, "index.html", {'login': 'true', 'login_success': str(succeeded).lower()})
+
+def create(req):
+    succeeded = True
+    u, p = req.POST['username'], req.POST['password']
+    for obj in Teacher.objects.all():
+        if obj.name == u and obj.password == p:
+            succeeded = False
+    req.post("/api/Teacher", data={'username': u, 'password': p})
+    return render(req, "index.html", {'creation': 'true', 'create_success': str(succeeded).lower()})
